ef6d46d2de5c1d91b988c902ffba9a272ebd9b6d
Devesh Goswami <devesh.g@simformsolutions.com>
2022-12-01 18:20:33 +0530

    feat: added updated feature in iOS

--- ios/EditContacts.m
+++ ios/EditContacts.m
@@ -12,7 +12,11 @@
 @interface RCT_EXTERN_MODULE(EditContacts,NSObject)
 
 RCT_EXTERN_METHOD(fetchContacts:(RCTResponseSenderBlock)resolve)
+
 RCT_EXTERN_METHOD(deleteContacts:(NSString*)nameToBeDeleted callback:(RCTResponseSenderBlock)callback)
+
 RCT_EXTERN_METHOD(addUserToContact:(NSString*)contactName contactNumber:(NSString*)contactNumber callback:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(updateContacts:(NSString*)contactName contactNumber:(NSString*)contactNumber callback:(RCTResponseSenderBlock)callback)
 
 @end
